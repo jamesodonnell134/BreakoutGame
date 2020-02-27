ba62f79fa8428ea074048aec9ead05654ce6b449
James O'Donnell CS2017 <james.o-donnell.2017@uni.strath.ac.uk>
2020-02-27 17:56:22 +0000

    [Unfinished] Complete up to stage 13/15 so far

---
+++ breakoutController.js
@@ -0,0 +1,12 @@
+function breakoutController() {
+    let view = new breakoutView();
+
+    this.init = function() {
+        view.init();
+    };
+}
+
+
+
+let controller = new breakoutController();
+window.addEventListener("load", controller.init);
